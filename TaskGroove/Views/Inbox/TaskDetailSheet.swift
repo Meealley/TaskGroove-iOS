f3649ae9bb575758c2371d770100ebb223856006
Oyewale Favour <oyewalefavour424@gmail.com>
2025-10-27 23:38:22 +0100

    Updated the Inboxviewmodel to capture persistence and edit task

---
+++ TaskGroove/Views/Inbox/TaskDetailSheet.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailSheet.swift
+//  TaskGroove
+//
+//  Created by Oyewale Favour on 27/10/2025.
+//
+
+import SwiftUI
+
+struct TaskDetailSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailSheet()
+}
