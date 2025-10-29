570bb0bc6e09452431b242355bd569120f1d2c72
Oyewale Favour <oyewalefavour424@gmail.com>
2025-10-29 05:03:22 +0100

    Modified the EditTaskSheet and Upcoming task view

---
+++ TaskGroove/Views/Upcoming/DailyTaskSection.swift
@@ -0,0 +1,18 @@
+//
+//  DailyTaskSection.swift
+//  TaskGroove
+//
+//  Created by Oyewale Favour on 28/10/2025.
+//
+
+import SwiftUI
+
+struct DailyTaskSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyTaskSection()
+}
